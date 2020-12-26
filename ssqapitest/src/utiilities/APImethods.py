c611e0f077a74c3318481046f638c4f6d5d7fe58
Bolofinde Olusegun <bolofbaba@MacBook-Pro-2.local>
2020-12-25 22:47:19 -0700

    payloads Implemtation

--- ssqapitest/src/utiilities/APImethods.py
+++ ssqapitest/src/utiilities/APImethods.py
@@ -1,16 +1,20 @@
-class APIMethodsImplementation(object):
+from requests_oauthlib import OAuth1
 from ssqapitest.src.utiilities.baseTest import BaseTest
+class APIMethodsImplementation(object):
+
     def __init__(self):
         self.endpoint = BaseTest.endpoints
         self.header = BaseTest.header
         self.access_key = BaseTest.auth["access_key"]
         self.access_password = BaseTest.auth["access_password"]
+        self.auth = OAuth1(self.access_key,self.access_password)
 
 
-    def post(self,endpoint,payloads,header,auth):
+    def post(self,endpoint=self.endpoint,payloads="",header=self.header,auth=self.auth):
         pass
 
     def update(self,endpoint=self.endpoint,payloads="",header=self.header,auth=self.auth):
+        pass
 
     def delete(self):
         pass
---
+++ ssqapitest/src/utiilities/payloads.py
@@ -0,0 +1,20 @@
+import json
+class APIPayloads(object):
+    """
+    payloads can be in form of :
+    excel file
+    json file
+    this class returns payload for different API endpoints
+    """
+
+    def __init__(self):
+        pass
+
+    def customer_payloads(self):
+        pass
+
+    def price_payloads(self):
+        pass
+
+    def products_payloads(self):
+        pass
