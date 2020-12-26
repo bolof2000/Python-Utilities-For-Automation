d12b020cad757432cec0084b7193de3b7022a0d6
Bolofinde Olusegun <bolofbaba@MacBook-Pro-2.local>
2020-12-25 22:01:16 -0700

    API-Methods

---
+++ ssqapitest/tests/customers/test_create_customers.py
@@ -0,0 +1,11 @@
+def test_create_customer():
+    """
+      create payload : excel, json, DB reusble
+      # send the payload to the endpoint : resuable
+     # verify the response  response data can be use elsewhere
+     # verify the status code
+     verify data in the response
+     verify data is created in the DB
+
+    """
+
---
+++ ssqapitest/tests/test_healthcheck.py
@@ -0,0 +1,4 @@
+import logging as logger
+
+def test_healthcheck():
+    logger.info("just running test")
